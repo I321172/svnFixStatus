03c7a6099acc2beee978269658c313616d94bdaa
I321172 <jason.guan01@sap.com>
2015-10-13 17:59:16 +0800

    packaging change to war

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -2,12 +2,14 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application 
+public class Application extends SpringBootServletInitializer
 {
     public static ApplicationContext context;
 
@@ -19,6 +21,11 @@ public class Application
         cache.setAllFeatures(dbUtil.getAllFeature());
     }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        SpringApplicationBuilder builder = application.sources(Application.class);
+        return builder;
+    }
 
 }
