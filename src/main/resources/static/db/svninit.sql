65988f47271d3e10a7277f59260a974795d4b520
I321172 <jason.guan01@sap.com>
2015-11-05 15:08:59 +0800

    update svninit.sql

--- src/main/resources/static/db/svninit.sql
+++ src/main/resources/static/db/svninit.sql
@@ -9,7 +9,8 @@ primary key(filepath,revision)
 create table if not exists filename(
 filepath varchar(600) primary key,
 filename varchar(80),
-}
+valid bit
+)
 
 create table if not exists revision(
 revision int(12) primary key,
